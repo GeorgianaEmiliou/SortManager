04a595cdc048afaf98c41a4e8872be968186e141
Georgiana <georgianaemiliou@gmail.com>
2022-08-12 11:57:01 +0100

    Fixed some issues in the Binary Sort class

--- src/main/java/com/sparta/ge/sorters/BinarySort.java
+++ src/main/java/com/sparta/ge/sorters/BinarySort.java
@@ -8,12 +8,11 @@ import java.util.Arrays;
 public class BinarySort implements Sorter {
     @Override
     public int[] sortArray(int[] arrayToSort) {
-        BinaryTree binaryTree = new BinaryTree(-1);
-        for(int i=0; i<arrayToSort.length; i++){
+        BinaryTree binaryTree = new BinaryTree(arrayToSort[0]);
+        for(int i=1; i<arrayToSort.length; i++){
             binaryTree.addElement(arrayToSort[i]);
         }
-        int[] sorted = binaryTree.getSortedTreeAsc();
-        int[] sortedArray = Arrays.copyOfRange(sorted, 1, sorted.length);
+        int[] sortedArray = binaryTree.getSortedTreeAsc();
         return sortedArray;
     }
 }
